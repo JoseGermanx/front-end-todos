25093ae6b055be186ed2dc5e5daa18e3214b0009
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-06 13:58:38 -0300

    Eliminar componente CreateToDo y ajustar espaciado en App.jsx

--- todos-react/src/App.jsx
+++ todos-react/src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
-import CreateToDo from './components/CreateToDo'
 import TodoList from './components/TodoList'
 
 function App() {
 
   return (
     <>
-    <CreateToDo />
+    
      <TodoList /> 
     </>
   )
