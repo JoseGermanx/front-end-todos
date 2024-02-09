91c87f95b9470d55c37847a20b09a45c80ed064c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-09 11:38:57 -0300

    Agrega scripts de Popper.js y Bootstrap y modifica la lista de tareas

--- todos-react/src/components/TodoList.jsx
+++ todos-react/src/components/TodoList.jsx
@@ -54,7 +54,6 @@ const TodoList = () => {
           <CreateToDo />
         </div>
         <div className="col-8">
-          
           <table className="table table-striped">
             <thead>
               <tr>
@@ -83,6 +82,14 @@ const TodoList = () => {
                     >
                       Delete
                     </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      Update
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -97,6 +104,42 @@ const TodoList = () => {
               )
             )}
           </span>
+        </div>
+      </div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modal title
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
